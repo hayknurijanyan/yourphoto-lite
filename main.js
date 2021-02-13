5e2abc00632ac22b90457b3c07cafe0610e3a889
Hayk <58630378+hayknurijanyan@users.noreply.github.com>
2021-02-14 01:06:59 +0400

    some changes

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const leftArrow = document.querySelector(".left-arrow")
 const rightArrow = document.querySelector(".right-arrow")
+const slider = document.querySelector("#slider")
 
+// console.log(slide.style.background)
+console.log(slider.style.backgroundImage)
 
-// leftArrow.style.backgroundColor = "red"
 
 function sliderRight() {
-    rightArrow.style.backgroundColor = "yellow"
-    console.log('done')
+    let sliderImage = window.getComputedStyle(slider).backgroundImage
+    let num = Number(sliderImage.charAt(37))
+    if (num === 3) {
+        num = 0
+    }
+    slider.style.backgroundImage = `url(slideshow/${num + 1}.jpg)`
+    console.log(slider)
 }
 
 rightArrow.addEventListener("click", sliderRight);
+
+function sliderLeft() {
+    let sliderImage = window.getComputedStyle(slider).backgroundImage
+    let num = Number(sliderImage.charAt(37))
+    if (num === 1) {
+        num = 4
+    }
+    slider.style.backgroundImage = `url(slideshow/${num - 1}.jpg)`
+    console.log(slider)
+}
+
+leftArrow.addEventListener("click", sliderLeft);
