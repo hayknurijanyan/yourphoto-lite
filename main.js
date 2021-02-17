84d8d33e9a1507716c313eef7b3b0537cfc38148
Hayk <58630378+hayknurijanyan@users.noreply.github.com>
2021-02-18 00:58:17 +0400

    new files added

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const leftArrow = document.querySelector(".left-arrow")
-const rightArrow = document.querySelector(".right-arrow")
-const slider = document.querySelector("#slider")
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const printProducts = document.querySelector(".print-products");
+// const productButton = document.querySelector(".prints-header-items")
+const productButton = document.getElementsByClassName("product-button");
+const slider = document.querySelector("#slider");
+let log = console.log
 
 // console.log(slide.style.background)
-console.log(slider.style.backgroundImage)
+// console.log(slider.style.backgroundImage)
 
 
 function sliderRight() {
@@ -25,7 +29,44 @@ function sliderLeft() {
         num = 4
     }
     slider.style.backgroundImage = `url(slideshow/${num - 1}.jpg)`
-    console.log(slider)
 }
 
 leftArrow.addEventListener("click", sliderLeft);
+
+
+
+for (let i = 0; i < productButton.length; i++) {
+    productButton[i].addEventListener("click", changeActive)
+}
+// for (let key of productButton) {
+//     key.addEventListener("click", changeActive)
+// }
+
+
+
+
+function changeActive(event) {
+    if (event.target.className.includes("active")) {
+        event.target.className = event.target.className.replace("active", "")
+    } else {
+        for (let i = 0; i < productButton.length; i++) {
+            productButton[i].className = productButton[i].className.replace("active", "");
+
+
+            for (key of printProducts.children) {
+                if (!event.target.className.includes(key.id)) {
+                    key.style.display = "none"
+                } else { key.style.display = "" };
+            }
+        }
+        event.target.className += " active"
+        // for (key of printProducts.children) {
+        //     if (event.target.className.includes(key.id))
+        //         key.style.display = "";
+
+        // }
+
+
+    }
+
+}
