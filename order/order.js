ff1718df4979e322e2d40c9e5bbb13447cd4fe07
Hayk <58630378+hayknurijanyan@users.noreply.github.com>
2021-02-19 00:58:57 +0400

    upload

--- order/order.js
+++ order/order.js
@@ -1,32 +1,39 @@
+const uploadBtn = document.querySelector(".upload-btn")
+const quantity = document.querySelector('.main-quantity')
 
-    const preview = document.querySelector('#preview');
-    const files   = document.querySelector('input[type=file]').files;
+
+uploadBtn.addEventListener("click", previewFiles)
 
 function previewFiles() {
+    const preview = document.querySelector('#preview');
+    const files = document.querySelector('input[type=file]').files;
 
     function readAndPreview(file) {
-  
-      // Make sure `file.name` matches our extensions criteria
-      if ( /\.(jpe?g|png)$/i.test(file.name) ) {
-        const reader = new FileReader();
-  
-        reader.addEventListener("load", function () {
-          let image = new Image();
-          image.height = 100;
-          image.title = file.name;
-          image.src = this.result;
-          preview.prepend( image );
-        }, false);
-  
-        reader.readAsDataURL(file);
-      } else {alert(`${file.name} is not supported format`)
-    console.log(file.name)
+
+        // Make sure `file.name` matches our extensions criteria
+        if (/\.(jpe?g|png)$/i.test(file.name)) {
+            const reader = new FileReader();
+
+            reader.addEventListener("load", function () {
+                let image = new Image();
+                image.height = 140;
+                image.title = `${Date.now()}${file.name}`;
+                console.log(image.title)
+                image.src = this.result;
+                preview.prepend(image);
+                quantity.innerHTML = `Quantity: ${preview.children.length}`
+
+            }, false);
+
+            reader.readAsDataURL(file);
+        } else {
+            alert(`${file.name} is not supported format`)
+        }
+
     }
-  
-    }
-  
+
     if (files) {
-      [].forEach.call(files, readAndPreview);
+        [].forEach.call(files, readAndPreview);
     }
-  
-  }
+
+}
