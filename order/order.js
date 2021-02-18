a41d6c41039a23f3cbe97338ff2147fba6dcc175
hayknurijanyn <[hayknurijanyan@gmail.com]>
2021-02-18 20:21:19 +0400

    som changes

--- order/order.js
+++ order/order.js
@@ -1,21 +1,32 @@
-const inpFile = document.getElementById("inpFile");
-const previewContainer = document.getElementById("imagePreview");
-const previewImage = previewContainer.querySelector(".image-preview-img");
-const previewText = previewContainer.querySelector(".image-preview-text")
-const log = console.log
 
-inpFile.addEventListener("change", function () {
-    const allFiles = this.files
-    const reader = new FileReader();
+    const preview = document.querySelector('#preview');
+    const files   = document.querySelector('input[type=file]').files;
 
-    if (allFiles.length > 0) {
+function previewFiles() {
 
-        reader.readAsDataURL(allFiles[0])
-
-        reader.onloadend = function () {
-            console.log('DONE', reader.readyState); // readyState will be 2
-        };
-        log(allFiles)
-        log(reader)
+    function readAndPreview(file) {
+  
+      // Make sure `file.name` matches our extensions criteria
+      if ( /\.(jpe?g|png)$/i.test(file.name) ) {
+        const reader = new FileReader();
+  
+        reader.addEventListener("load", function () {
+          let image = new Image();
+          image.height = 100;
+          image.title = file.name;
+          image.src = this.result;
+          preview.prepend( image );
+        }, false);
+  
+        reader.readAsDataURL(file);
+      } else {alert(`${file.name} is not supported format`)
+    console.log(file.name)
     }
-})
+  
+    }
+  
+    if (files) {
+      [].forEach.call(files, readAndPreview);
+    }
+  
+  }
